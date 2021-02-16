3b6ebe20907c4029d4a75bd371c1dfdb21a6b9b8
chvsuresh21 <chvsuresh21@gmail.com>
2021-02-16 13:22:33 +0530

    Updated Program.cs

--- Trainings/Program.cs
+++ Trainings/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("This is test code");
+            Console.WriteLine("This is test code 123");
             Console.ReadLine();
         }
     }
